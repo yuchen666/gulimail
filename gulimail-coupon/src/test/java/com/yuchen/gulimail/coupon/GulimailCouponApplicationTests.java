c95206fa7d0ba6050e15736d0ed4c83f5030667c
yuchen666 <1215852624@qq.com>
2020-04-15 18:38:19 +0800

    项目初始结构创建

---
+++ gulimail-coupon/src/test/java/com/yuchen/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuchen.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-ware/src/test/java/com/eyuchen/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyuchen.gulimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
